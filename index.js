6d191cdfe7a28c52b68a0beaa49f5fec98939560
sauravsaurav <praj5354@gmail.com>
2023-03-04 12:27:34 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+var format = require('date-format');
+
+const app = express();
+
+const PORT = 4000 || process.env.PORT;
+
+
+app.get("/api/v1/:token",(req, res)=>{
+    console.log(req.params.token);
+    res.status(200).json({param : req.params.token});
+})
+
+
+app.get("/",(req,res)=>{
+    res.status(200).send("<h1>Hello World</h1>");
+});
+
+
+app.get("/api/v1/instagram",(req , res)=>{
+    const instaSocial = {
+        username : "sauravkumar411",
+        followers : 66,
+        follow : 70,
+        date : format.asString('hh:mm:ss.SSS', new Date())
+    };
+
+    return res.status(200).json(instaSocial)
+});
+
+
+app.get("/api/v1/facebook",(req , res)=>{
+    const instaSocial = {
+        username : "saurav3",
+        followers : 60,
+        follow : 75,
+        date : format.asString('hh:mm:ss.SSS', new Date())
+    };
+
+    return res.status(200).json(instaSocial)
+});
+
+
+app.get("/api/v1/linkdin",(req , res)=>{
+    const instaSocial = {
+        username : "sauravKumar",
+        followers : 88,
+        follow : 90,
+        date : format.asString('hh:mm:ss.SSS', new Date())
+    }; 
+
+    return res.status(200).json(instaSocial)
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running at ${PORT}`);
+})
